962321b494064b17e593d8571b73df188ed7f9a6
Viren Khatri <vk102002@gmail.com>
2024-06-03 00:10:22 +0530

    feat: added auth to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,40 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.name),
   });
 
-  console.log(images);
+  return (
+    <div className="flex flex-wrap justify-center gap-4 align-middle">
+      {images.map((image, index) => (
+        <div
+          className="my-auto flex w-48 flex-col gap-2"
+          key={image.id + `-${index++}`}
+        >
+          <img src={image.url} alt="image" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap justify-center gap-4 align-middle">
-        {images.map((image, index) => (
-          <div
-            className="my-auto flex w-48 flex-col gap-2"
-            key={image.id + `-${index++}`}
-          >
-            <img src={image.url} alt="image" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="flex flex-col items-center justify-center gap-4 p-4">
+          Please sign in to view images
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
