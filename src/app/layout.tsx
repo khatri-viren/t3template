3778b4a6d9dbf387dde9d2837e1d954bbea03c91
Viren Khatri <vk102002@gmail.com>
2024-06-03 13:57:23 +0530

    feat: initialized shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en" className={`${GeistSans.variable}`}>
+      <html lang="en" className={`${GeistSans.variable} dark`}>
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <body>
           <div className="grid h-screen grid-rows-[auto,1fr] ">
