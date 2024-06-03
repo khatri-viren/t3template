46c252760b43226abd12414d75131b59473a0b13
Viren Khatri <vk102002@gmail.com>
2024-06-03 18:36:21 +0530

    feat: upload persmission to only limited users

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,6 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { db } from "~/server/db";
 import { images } from "~/server/db/schema";
 import { ratelimit } from "~/server/ratelimit";
@@ -11,8 +11,12 @@ export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 5 } })
     .middleware(async ({ req }) => {
       const user = auth();
-
       if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      const fullUserData = await clerkClient.users.getUser(user.userId);
+      if (fullUserData?.privateMetadata?.["can-upload"] !== "true")
+        throw new UploadThingError("User does not have permission to upload");
+
       const { success } = await ratelimit.limit(user.userId);
       if (!success) throw new UploadThingError("Too many requests");
 
